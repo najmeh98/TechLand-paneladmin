2a6ccffff7232c330a30384468d714160d3080b8
najmeh98 <foroughi.najemh98@gmail.com>
2022-07-15 20:24:40 +0430

    add custom Input

---
+++ components/CustomInput.tsx
@@ -0,0 +1,173 @@
+import React, {
+  ChangeEvent,
+  DetailedHTMLProps,
+  TextareaHTMLAttributes,
+  useState,
+} from "react";
+import styled, { css } from "styled-components";
+import { useTheme } from "./Context/ThemeContext";
+import { FormItem } from "./share/Container";
+import { Space } from "./share/Space";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
+interface TextInput {
+  type: "text";
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+interface FileInput {
+  type: "file";
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+interface TextArea {
+  type: "textarea";
+  onChange?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+interface TextPassword {
+  type: "password";
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export type InputCommonProps = {
+  label?: string;
+  placeholder?: string;
+  value?: string;
+  width?: string;
+  height?: string;
+  enctype?: string | undefined;
+  name?: string;
+  style?: any;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit?: (() => void) | undefined;
+  type?: string;
+};
+
+type Props = (TextInput | TextArea | FileInput | TextPassword) &
+  InputCommonProps;
+
+interface ImgProps {
+  password?: boolean;
+  onClick: () => void;
+}
+
+export const CustomInput: React.FC<Props> = ({
+  label,
+  placeholder,
+  value,
+  width,
+  name,
+  height,
+  enctype,
+  onChange,
+  style,
+  onSubmit,
+  type,
+}) => {
+  const [show, setShow] = useState(false);
+
+  let t = useTheme();
+
+  return (
+    <FormItem
+      // method="posts"
+      // enctype={enctype}
+      style={{
+        marginTop: t.margin.medium,
+        marginBottom: t.margin.medium,
+        width: width,
+        height: height,
+        direction: "ltr",
+        ...style,
+      }}
+    >
+      {label && (
+        <>
+          <label
+            style={{
+              color: t.color.labelColor,
+              paddingTop: t.padding.normal,
+              paddingBottom: t.padding.normal,
+            }}
+          >
+            {label}
+          </label>
+        </>
+      )}
+      {type === "password" && (
+        <Input
+          value={value}
+          onChange={onChange}
+          type={show ? "text" : "password"}
+          placeholder={placeholder}
+          style={{ padding: t.padding.normal, height: t.height.small }}
+        />
+      )}
+      {!(type === "password" || type === "textarea") && (
+        <Input
+          name={name}
+          value={value}
+          onChange={onChange}
+          type={type}
+          placeholder={placeholder}
+          style={{ padding: t.padding.normal, height: t.height.small }}
+        />
+      )}
+
+      {type === "textarea" && (
+        <textarea
+          value={value}
+          onChange={onChange}
+          rows={6}
+          style={{
+            borderColor: t.color.borderColor,
+            resize: "none",
+            padding: t.padding.normal,
+          }}
+        ></textarea>
+      )}
+
+      {type === "password" && (
+        <div style={{ position: "relative" }}>
+          <ShowPass onClick={() => setShow(!show)} password>
+            {!show && <FiEye />} {show && <FiEyeOff />}
+          </ShowPass>
+        </div>
+      )}
+    </FormItem>
+  );
+};
+
+export const Input = styled.input`
+  outline: none;
+  border: 1px solid rgb(204, 204, 204);
+  direction: ltr !important;
+  display: flex;
+  /* text-align: left; */
+  border-radius: 7px;
+  width: 100%;
+  margin-bottom: 5px;
+  /* margin-top: 5px; */
+  resize: none;
+  &::placeholder {
+    font-size: 13px;
+  }
+`;
+
+const ShowPass = styled.div<ImgProps>`
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  right: 0px;
+  top: -45px;
+  /* direction: rtl; */
+  cursor: pointer;
+  ${(p) =>
+    p.password &&
+    css`
+      border-left: 1px solid rgb(204, 204, 204);
+      cursor: pointer;
+    `}
+  svg {
+    display: block;
+    margin: 10px auto;
+  }
+`;
