f6e7b752a47fbfd0ed294926a323b80a6ca94560
najmeh98 <foroughi.najemh98@gmail.com>
2022-07-18 17:39:14 +0430

    add global style

---
+++ components/share/Container.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const FormItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* align-items: flex-start; */
+  justify-content: flex-start;
+
+  direction: rtl;
+`;
+
+export const FlexRow = styled.div`
+  display: flex;
+  /* align-items: center; */
+  justify-content: space-between;
+`;
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
