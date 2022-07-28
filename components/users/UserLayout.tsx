cf08d3f1b2dfba18b199e00976a790e077879fe5
najmeh98 <foroughi.najemh98@gmail.com>
2022-07-29 00:06:40 +0430

    add Layout component

---
+++ components/users/UserLayout.tsx
@@ -0,0 +1,60 @@
+import { useRouter } from "next/router";
+import React from "react";
+import styled from "styled-components";
+import { useTheme } from "../Context/ThemeContext";
+import { HeaderText } from "../HeaderText";
+import { Flex, FlexRow, MainWrapper } from "../share/Container";
+import { SidebarOption } from "../Sidebar/SidebarOption";
+import { UserProp } from "./type";
+
+interface Prop {
+  width: string;
+}
+
+export const UserLayout: React.FC<UserProp> = ({
+  title,
+  children,
+  width,
+  style,
+}) => {
+  const t = useTheme();
+  const router = useRouter();
+
+  return (
+    <FlexRow>
+      <SidebarOption />
+      <MainWrapper>
+        <Conianer>
+          <HeaderText
+            style={{ display: "flex", alignItems: "flex-start", width: "70%" }}
+          >
+            {title}
+          </HeaderText>
+          <Wrapper style={{ width: width ? `${width}` : "100%", ...style }}>
+            {children}
+          </Wrapper>
+        </Conianer>
+      </MainWrapper>
+    </FlexRow>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid rgb(230, 235, 235);
+  border-radius: 16px;
+  box-shadow: rgb(16 30 115 / 6%) 0px 6px 26px 0px;
+  /* padding: 30px; */
+  /* height: 100vh; */
+  margin: 10px 0px;
+  /* height: calc(100vh - 130px); */
+`;
+
+const Conianer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
