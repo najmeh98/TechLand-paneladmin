a702a5c6d355672d60d77555c6152045cf3e5f2c
najmeh98 <foroughi.najmeh98@gmail.com>
2022-08-03 00:08:47 +0430

    update data

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  experimental: {
+    externalDir:
+      true |
+      {
+        enabled: true,
+        silent: true,
+      },
+  },
+};
