975c2c85319b27cf9076eab1c58b129fb6888361
najmeh98 <foroughi.najmeh98@gmail.com>
2022-08-22 10:44:33 +0430

    Checking the authentication of admins

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,35 +4,29 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useAppContext } from "../components/AppManag.tsx/AppContext";
+import Dashboard from "../components/Dashboard";
 import { Layout } from "../components/register/Layout";
 import { RegisterHeader } from "../components/register/RegisterHeader";
 import { SidebarOption } from "../components/Sidebar/SidebarOption";
 import styles from "../styles/Home.module.css";
 import Step1 from "./auth/register";
+import { useDropzone } from "react-dropzone";
 
 const Home: NextPage = () => {
-  const { logout } = useAppContext();
   const router = useRouter();
+
   useEffect(() => {
-    const token = localStorage.getItem("admintoken");
-    const email = localStorage.getItem("adminemail");
-    console.log("token", token);
-
-    if (!token || token == undefined) {
-      console.log("token is undefind");
-      router.push("/auth/loginByEmail");
-      return;
+    if (typeof window == "object") {
+      if (window.localStorage.getItem("$adnTK") === null) {
+        router.push("/auth/loginByEmail");
+        return;
+      }
     }
-
-    if (!email || email == undefined) {
-      router.push("/auth/loginByEmail");
-      return;
-    }
-    console.log("done");
   }, [router]);
+
   return (
     <>
-      <SidebarOption />
+      <Dashboard />
     </>
   );
 };
