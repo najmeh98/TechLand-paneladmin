92a77a862580c0895fa3dce906233a7e087ad173
najmeh98 <foroughi.najmeh98@gmail.com>
2022-08-30 23:48:14 +0430

    add icon component

--- components/icons/Icon.tsx
+++ components/icons/Icon.tsx
@@ -33,3 +33,41 @@ const Svg = styled.svg`
   transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   cursor: pointer;
 `;
+
+export const SaveList = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    aria-label="Lists"
+  >
+    <path
+      d="M4.5 6.25V21c0 .2.24.32.4.2l5.45-4.09a.25.25 0 0 1 .3 0l5.45 4.09c.16.12.4 0 .4-.2V6.25a.25.25 0 0 0-.25-.25H4.75a.25.25 0 0 0-.25.25z"
+      stroke="currentColor"
+    ></path>
+    <path
+      d="M8 6V3.25c0-.14.11-.25.25-.25h11.5c.14 0 .25.11.25.25V16.5"
+      stroke="currentColor"
+    ></path>
+  </svg>
+);
+
+export const Writepost = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    aria-label="Write"
+  >
+    <path
+      d="M14 4a.5.5 0 0 0 0-1v1zm7 6a.5.5 0 0 0-1 0h1zm-7-7H4v1h10V3zM3 4v16h1V4H3zm1 17h16v-1H4v1zm17-1V10h-1v10h1zm-1 1a1 1 0 0 0 1-1h-1v1zM3 20a1 1 0 0 0 1 1v-1H3zM4 3a1 1 0 0 0-1 1h1V3z"
+      fill="currentColor"
+    ></path>
+    <path
+      d="M17.5 4.5l-8.46 8.46a.25.25 0 0 0-.06.1l-.82 2.47c-.07.2.12.38.31.31l2.47-.82a.25.25 0 0 0 .1-.06L19.5 6.5m-2-2l2.32-2.32c.1-.1.26-.1.36 0l1.64 1.64c.1.1.1.26 0 .36L19.5 6.5m-2-2l2 2"
+      stroke="currentColor"
+    ></path>
+  </svg>
+);
