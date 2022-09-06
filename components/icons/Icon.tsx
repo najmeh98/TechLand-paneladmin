577b164c7b731fc8edc41e93bc9bd452d7afe6cc
najmeh98 <foroughi.najmeh98@gmail.com>
2022-09-06 22:57:32 +0430

    Added arrow down icon

--- components/icons/Icon.tsx
+++ components/icons/Icon.tsx
@@ -71,3 +71,18 @@ export const Writepost = () => (
     ></path>
   </svg>
 );
+
+export const ArrowIcon = ({ fill, style }: { fill: string; style: any }) => (
+  <svg
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    fill={fill}
+    style={style}
+    // class=""
+    // style="left: calc(20px);"
+  >
+    <g>
+      <path d="M12.538 6.478c-.14-.146-.335-.228-.538-.228s-.396.082-.538.228l-9.252 9.53c-.21.217-.27.538-.152.815.117.277.39.458.69.458h18.5c.302 0 .573-.18.69-.457.118-.277.058-.598-.152-.814l-9.248-9.532z"></path>
+    </g>
+  </svg>
+);
