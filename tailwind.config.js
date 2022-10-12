c081afc0eb59a3e166b461c3779fdbd30a466480
najmeh98 <foroughi.najmeh98@gmail.com>
2022-10-12 11:08:46 +0330

    Add theme in  tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,49 @@
 /** @type {import('tailwindcss').Config} */
+// const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      maxsm: { max: "480px" },
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1200px",
+    },
+
+    // screens: {
+    //   sm: { max: "640px" },
+    //   // => @media (min-width: 640px) { ... }
+
+    //   md: { max: "1000px" },
+    //   // => @media (min-width: 768px) { ... }
+
+    //   lg: "1024px",
+    //   // => @media (min-width: 1024px) { ... }
+
+    //   xl: "1280px",
+    //   // => @media (min-width: 1280px) { ... }
+
+    //   "2xl": "1536px",
+    //   // => @media (min-width: 1536px) { ... }
+    // },
+    extend: {
+      fontFamily: {
+        sans: [
+          "source-serif-pro",
+          "Georgia",
+          "Cambria",
+          "Times New Roman",
+          "Times",
+          "serif",
+        ],
+      },
+      width: {},
+    },
   },
   plugins: [],
 };
