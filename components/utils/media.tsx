d26732b8da42b5acff4a1f3c603657742f6bfbcf
najmeh98 <foroughi.najmeh98@gmail.com>
2022-10-23 11:04:32 +0330

    Add media query

--- components/utils/media.tsx
+++ components/utils/media.tsx
@@ -1,10 +1,10 @@
 import { css, FlattenSimpleInterpolation } from "styled-components";
 
-export const tablet = (body: any): FlattenSimpleInterpolation => css`
-  @media (max-width: 950px) and (max-width: 1024px) {
-    ${body};
-  }
-`;
+// export const tablet = (body: any): FlattenSimpleInterpolation => css`
+//   @media (max-width: 950px) and (max-width: 1024px) {
+//     ${body};
+//   }
+// `;
 
 export const mobile = (body: any) => css`
   @media (max-width: 768px) {
@@ -20,6 +20,12 @@ export const notmobile = (inner: any) => css`
 
 export const desktop = (body: any) => css`
   @media (min-width: 1024px) {
+    ${body};
+  }
+`;
+
+export const tablet = (body: any) => css`
+  @media (max-width: 976px) {
     ${body};
   }
 `;
