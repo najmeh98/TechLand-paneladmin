4ade2be69479cf36c198a01a22473d778c873adb
najmeh98 <foroughi.najmeh98@gmail.com>
2022-12-06 12:09:15 +0330

    Add interface ts

---
+++ components/types/postCreate.interface.ts
@@ -0,0 +1,24 @@
+export interface OwnProp {
+  title: string;
+  content: string;
+}
+
+export interface CatItem {
+  id: string;
+  name: string;
+  description?: string | undefined;
+  image: string;
+}
+
+export interface OwnPost {
+  length: number;
+  id: string;
+  title: string;
+  content: string;
+  image: string;
+  created: string;
+  update: string;
+  category: {
+    name: string;
+  };
+}
