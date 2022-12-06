1f4478b6f7c79778dce35038116e28146b883647
najmeh98 <foroughi.najmeh98@gmail.com>
2022-12-06 12:05:03 +0330

    Add user interface Ts

---
+++ components/types/type.ts
@@ -0,0 +1,10 @@
+export interface InfoProp {
+  name: string;
+  family: string;
+  email: string;
+  username: string;
+  phoneNumber: number;
+  address: string;
+  skill?: string;
+  bio?: string;
+}
