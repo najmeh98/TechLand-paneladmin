50c6fbac24fb82cddf3f22be610c9b384cf3b6f0
najmeh98 <foroughi.najmeh98@gmail.com>
2022-12-06 12:07:42 +0330

    Add interface ts

---
+++ components/types/adprofiletype.ts
@@ -0,0 +1,11 @@
+export interface ProfileProp {
+  id?: undefined;
+  name: string;
+  family: string;
+  username: string;
+  address: string;
+  email: string;
+  phoneNumber: number;
+  bio?: string;
+  job?: string;
+}
